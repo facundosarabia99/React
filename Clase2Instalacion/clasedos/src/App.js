f607cf543ac53a492d7253fa8d767e5ec35c8263
facundosarabia99 <facundosarabia@gmail.com>
2022-04-27 15:32:56 -0300

    .

--- Clase2Instalacion/clasedos/src/App.js
+++ Clase2Instalacion/clasedos/src/App.js
@@ -6,4 +6,40 @@ function App() {
   );
 }
 
+const objeto = {
+  id: 1,
+  nombre: "Agustin"
+}
+
+const { nombre } = objeto;
+
+console.log(nombre)
+
+const array3 = [
+  {
+    id: 1,
+    title: "Agustin"
+  },
+  {
+    id: 2,
+    title: "Juan"
+  }
+]
+
+const { title } = array3[1];
+
+console.log(title)
+
+const contenedorStyle = {
+  backgroundColor: "#ccc"
+}
+
+//return (
+  //<div style={contenedorStyle} className="App">
+    //<h1>Lista primer aplicacion</h1>
+    //<p>Accedo a la propiedad Title del objeto en el array con posiscion 1 {title}</p>
+    //<p> Accedo a la prioridad </p>
+  //</div>
+//)
+
 export default App;
