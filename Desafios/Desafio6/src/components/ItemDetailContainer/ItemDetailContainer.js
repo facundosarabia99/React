e294bac88ba87f3a78757eed35ea4671e40109ca
facundosarabia99 <facundosarabia@gmail.com>
2022-06-03 17:27:27 -0300

    .

--- Desafios/Desafio6/src/components/ItemDetailContainer/ItemDetailContainer.js
+++ Desafios/Desafio6/src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,23 +1,34 @@
-import {products} from "../../data/products"
+// import {products} from "../../data/products"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import React from "react"
 import { useParams } from "react-router-dom"
+import { getFirestore, doc, getDoc } from "firebase/firestore"
 
 export default function ItemDetailtContainer(){
 
     const { id } = useParams()
     
     const[item, setItem] = React.useState([])
+    React.useEffect(() => {
+        const db = getFirestore()
+        // Traer Producto
+        const porductRef = doc(db, "products", productId)
+        getDoc(porductRef).then(snapshot => {
+            if(snapshot.exists()){
+           setItem({id: snapshot.id, ...snapshot.data()})
+            }
+        })
+    }, [])
     
-    const getItem = new Promise((resolve,reject) =>{
-        setTimeout(() =>{
-            resolve(products);
-        },1)
-    }).then(() => {
-        console.log("getItem")
-        const product = products.find(product => product.id == id);
-        setItem(product);
-    }) 
+    // const getItem = new Promise((resolve,reject) =>{
+    //     setTimeout(() =>{
+    //         resolve(products);
+    //     },1)
+    // }).then(() => {
+    //     console.log("getItem")
+    //     const product = products.find(product => product.id == id);
+    //     setItem(product);
+    // }) 
 
 
     return (
--- Desafios/Desafio6/src/components/ItemListContainer/ItemListContainer.js
+++ Desafios/Desafio6/src/components/ItemListContainer/ItemListContainer.js
@@ -5,30 +5,49 @@ import {products} from "../../data/products"
 import ItemList from '../ItemList/ItemList';
 import React, { useEffect } from "react"
 import { useParams } from "react-router-dom"
-import { doc, getDoc, getFirestore} from "firebase/firestore";
+import { doc, getDoc, getDocs, collection, getFirestore} from "firebase/firestore";
 import { Container, Spinner, Row, Col } from 'react-bootstrap';
 import { useState } from "react";
+import { getFirestore, getDoc, doc } from "firebase/firestore";
 
 
 export default function ItemListContainer({title, categoryId}){
 
     const[items, setItems] = React.useState([]);
-    const[isLoading, setIsLoading] = React.useState(0);
+    const[isLoading, setIsLoading] = React.useState(false);
 
-    useEffect( () => {
-        if(categoryId){
-            setTimeout(() =>{
-                setIsLoading(false);
-            }, 1000);
-            setItems(products.filter(product => product.categoryId === +categoryId));
-        }
-        else{
-            setTimeout(() =>{
-                setIsLoading(false);
-            }, 1000);
-            setItems(products);
-        }
-    }, [categoryId, isLoading]);
+    React.useEffect(() => {
+        const db = getFirestore()
+        const porductRef = doc(db, "products", "YXeivmu71plXq2smtaao")
+        getDoc(porductRef).then(snapshot => {
+            if(snapshot.exists()){
+            console.log(snapshot.id)
+            console.log(snapshot.data())
+            }
+        })
+
+        const porductsRef = collection(db, "products")
+        getDocs(porductsRef).then(snapshots => {
+           setItems(snapshots.docs.map(doc => ({id: doc.id, ...doc.data()})))
+        })
+
+
+    },[])
+
+    // useEffect( () => {
+    //     if(categoryId){
+    //         setTimeout(() =>{
+    //             setIsLoading(false);
+    //         }, 1000);
+    //         setItems(products.filter(product => product.categoryId === +categoryId));
+    //     }
+    //     else{
+    //         setTimeout(() =>{
+    //             setIsLoading(false);
+    //         }, 1000);
+    //         setItems(products);
+    //     }
+    // }, [categoryId, isLoading]);
     
     return (
         <Container className={"itemlist-container"}>
