dba27c2f02a881912cdf8079f8b1d766cdd9519a
facundosarabia99 <facundosarabia@gmail.com>
2022-06-03 16:32:10 -0300

    .

--- Desafios/Desafio6/src/components/ItemListContainer/ItemListContainer.js
+++ Desafios/Desafio6/src/components/ItemListContainer/ItemListContainer.js
@@ -5,45 +5,44 @@ import {products} from "../../data/products"
 import ItemList from '../ItemList/ItemList';
 import React, { useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { doc, getDoc, getFirestore} from "firebase/firestore";
+import { Container, Spinner } from 'react-bootstrap';
 
 
-export default function ItemListContainer({greeting}){
+export default function ItemListContainer({title, categoryId}){
 
-debugger;
-    const { id } = useParams()
-    const[productsItems, getProducts] = React.useState([])
+    const[items, setItems] = React.useState([]);
+    const[isLoading, setIsLoading] = React.useState(0);
 
     useEffect( () => {
-        const getProductsTask = new Promise((resolve,reject) =>{
+        if(categoryId){
             setTimeout(() =>{
-                resolve(getProducts(products));
-            },3000)
-        })
-        getProductsTask.then((products) =>{
-            const product = products.find(product => product.id == id);
-            getProducts(product);
-        })
-    })
+                setIsLoading(false);
+            }, 1000);
+            setItems(products.filter(product => product.categoryId === +categoryId));
+        }
+        else{
+            setTimeout(() =>{
+                setIsLoading(false);
+            }, 1000);
+            setItems(products);
+        }
+    }, [categoryId, isLoading]);
     
-    const addTocart = (count) =>{
-        console.log(`add to cart ${count} items`);
-        toast.success(`add to cart ${count} items!`, {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            });
-    }
     return (
-        <>           
-            <Alert key='success' variant='success'>
-                {greeting}
-            </Alert>
-            {/* <ItemCount stock='5' initial='1' onAdd={addTocart}/> */}
-            <ItemList items = {productsItems} onAdd={addTocart}/>
-        </>   
-    )
+        <Container className={"itemlist-container"}>
+            <Row>
+                <Col>
+                <h1>{title}</h1>
+                </Col>
+            </Row>
+            <Row>
+                {isLoading ? (
+                    <Spinner animation="border" role="status" />
+                ) : (
+                    <ItemList items={items} />
+                )}
+            </Row>
+        </Container>
+    );
 }
